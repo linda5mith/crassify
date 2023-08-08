043846c6c600e2641856b1255f497af120d7351d
Linda Smith <linda.jsm@outlook.com>
2023-08-08 17:07:32 +0100

    phylip file created

--- crassify.py
+++ crassify.py
@@ -17,7 +17,7 @@ def map_genome_to_proteome(path_to_prots):
         prots = list_files(path_to_prots)
         if prots:
             console = Console()
-            console.log('Mapping genome ID to protein ID\'s.', style="bold green")
+            console.log('Mapping genome ID to protein ID\'s.', style="bold purple")
             proteins={}
             genome_lens={}
             for seq in track(prots, description='Parsing proteins...'):
@@ -47,7 +47,7 @@ def map_genome_to_proteome(path_to_prots):
 def merge_metadata_matches(matches, input_metadata, db_metadata):
     '''Merges protein metadata with diamond output matches.tsv'''
     console = Console()
-    console.log('Merging metadata with matches.tsv protein alignments.',style="bold green")
+    console.log('Merging metadata with matches.tsv protein alignments.',style="bold purple")
     with Progress() as pb:
         t1 = pb.add_task(description='Merging...', total=100)
         matches = pd.read_csv(matches, sep='\t', header=None, low_memory=False)
@@ -68,101 +68,101 @@ def calc_dist(df):
     Returns distance matrix based on formula 1-(ORFs_A)+(ORFs_B)/(len(Genome_A+Genome_B)/2)
     '''
     console = Console()
-    console.log('Calculating distances between genomes.',style="bold green")
+    console.log('Calculating distances between genomes.',style="bold purple")
     now = datetime.now()
     dt_string = now.strftime("%d.%m.%Y_%H:%M")
     outpath = f'./crassify_{dt_string}'
     try:
         os.mkdir(outpath)
+        with Progress() as pb:
+            t1 = pb.add_task('Finding hits...', total=100)
+            df['conserved_AA_#'] = (df['length']/100)*df['pident']
+            # taxonomy for each protein hit
+            df_proteins = df.sort_values(by=['qseqid','sseqid','sstart','send']) \
+                .loc[df.reset_index().groupby(['qseqid'])['conserved_AA_#'].idxmax()] \
+                .sort_values(by=['qseqid','conserved_AA_#']) \
+                .reset_index(drop=True) \
+                .to_csv(f'{outpath}/protein_hits.csv',index=False)
+            pb.update(task_id=t1, advance=33.33)
+            # return top 5 genome hits 
+            df_genome_hits = df.groupby(['qseqid_ID','genome_ncbi_acc','virus'])[['conserved_AA_#','length']].sum().reset_index() \
+                .sort_values(by=['conserved_AA_#'], ascending=False) \
+                .groupby('qseqid_ID').head(5).reset_index(drop=True) \
+                .merge(df, left_on=['genome_ncbi_acc'], right_on=['genome_ncbi_acc'], how='left', suffixes=('', '_y')) \
+                .drop_duplicates(keep='first').reset_index(drop=True) \
+                .groupby(['qseqid_ID','genome_ncbi_acc'])[['definition','qseqid','sseqid','conserved_AA_#_y','pident','length_y']].agg(lambda x: list(x)).reset_index() \
+                .merge(df, left_on=['genome_ncbi_acc'], right_on=['genome_ncbi_acc'], how='left', suffixes=('', '_y')) \
+                .drop_duplicates(subset=['qseqid_ID','genome_ncbi_acc'], keep='first').reset_index(drop=True)
+            df_genome_hits = df_genome_hits.drop(df_genome_hits.filter(regex='_y$').columns, axis=1) \
+                .drop('conserved_AA_#', axis=1) \
+                .to_csv(f'{outpath}/genome_hits.csv',index=False)
+            pb.update(task_id=t1, advance=33.33)
+            # calculate distances 
+            df_dist = df.groupby(['qseqid_ID','genome_ncbi_acc']).agg({'conserved_AA_#':'sum', 
+                                        'qseqid_genome_length':'first',
+                                        'sseqid_genome_length':'first',
+                                        'virus':'first',
+                                        'length':'sum'}).reset_index()
+            df_dist['distance'] = 1-(df_dist['conserved_AA_#']*3)/((df_dist['qseqid_genome_length']+df_dist['sseqid_genome_length'])/2)
+            df_dist = df_dist.sort_values(by=['qseqid_ID','distance']) \
+                .reset_index(drop=True) \
+                .round(6) \
+                .to_csv(f'{outpath}/dists.csv',index=False)
+            pb.update(task_id=t1, advance=34)
     except Exception as e:
         print(e)
-    with Progress() as pb:
-        t1 = pb.add_task('Finding hits...', total=100)
-        df['conserved_AA_#'] = (df['length']/100)*df['pident']
-        # taxonomy for each protein hit
-        df_proteins = df.sort_values(by=['qseqid','sseqid','sstart','send']) \
-            .loc[df.reset_index().groupby(['qseqid'])['conserved_AA_#'].idxmax()] \
-            .sort_values(by=['qseqid','conserved_AA_#']) \
-            .reset_index(drop=True) \
-            .to_csv(f'{outpath}/protein_hits.csv',index=False)
-        pb.update(task_id=t1, advance=33.33)
-        # return top 5 genome hits 
-        df_genome_hits = df.groupby(['qseqid_ID','genome_ncbi_acc','virus'])[['conserved_AA_#','length']].sum().reset_index() \
-            .sort_values(by=['conserved_AA_#'], ascending=False) \
-            .groupby('qseqid_ID').head(5).reset_index(drop=True) \
-            .merge(df, left_on=['genome_ncbi_acc'], right_on=['genome_ncbi_acc'], how='left', suffixes=('', '_y')) \
-            .drop_duplicates(keep='first').reset_index(drop=True) \
-            .groupby(['qseqid_ID','genome_ncbi_acc'])[['definition','qseqid','sseqid','conserved_AA_#_y','pident','length_y']].agg(lambda x: list(x)).reset_index() \
-            .merge(df, left_on=['genome_ncbi_acc'], right_on=['genome_ncbi_acc'], how='left', suffixes=('', '_y')) \
-            .drop_duplicates(subset=['qseqid_ID','genome_ncbi_acc'], keep='first').reset_index(drop=True)
-        df_genome_hits = df_genome_hits.drop(df_genome_hits.filter(regex='_y$').columns, axis=1) \
-            .drop('conserved_AA_#', axis=1) \
-            .to_csv(f'{outpath}/genome_hits.csv',index=False)
-        pb.update(task_id=t1, advance=33.33)
-        # calculate distances 
-        df_dist = df.groupby(['qseqid_ID','genome_ncbi_acc']).agg({'conserved_AA_#':'sum', 
-                                    'qseqid_genome_length':'first',
-                                    'sseqid_genome_length':'first',
-                                    'virus':'first',
-                                    'length':'sum'}).reset_index()
-        df_dist['distance'] = 1-(df_dist['conserved_AA_#']*3)/((df_dist['qseqid_genome_length']+df_dist['sseqid_genome_length'])/2)
-        df_dist = df_dist.sort_values(by=['qseqid_ID','distance']) \
-            .reset_index(drop=True) \
-            .round(6) \
-            .to_csv(f'{outpath}/dists.csv',index=False)
-        pb.update(task_id=t1, advance=34)
-    return df_dist 
+    return f'{outpath}/dists.csv' 
 
-def to_phylip(df_dist):
+def to_phylip(df_dist, ictv_dists):
     '''Converts distances to PHYLIP format.'''
     console = Console()
-    console.print(f'{datetime.now()}: Converting distance matrix to PHYLIP format.')
-    matrix = df_dist.pivot_table(columns='qseqid_ID', index='virus', values='distance').reset_index()
-    matrix = matrix.set_index('virus')
-    #matrix.to_csv('dist_matrix_phylim_raw.dist', header=True, index=True, sep =' ')
-    # Check dimensions of matrix
-    cols = dist_piv.columns.to_list()
-    indx = dist_piv.index.to_list()
-    cols_missing = list(set(indx) - set(cols))
-    idx_missing = list(set(cols) - set(indx))
-    pbar.update(50)
-    if len(cols)>len(indx):
-        # insert extra column values into index
+    console.log('Converting distance matrix to PHYLIP format.',style="bold purple")
+    with Progress() as pb:
+        t1 = pb.add_task('Making matrix...', total=100)
+        outpath = df_dist.split('/')[0]
+        df_dist = pd.read_csv(df_dist)
+        ictv_dists = pd.read_csv(ictv_dists)
+        df_dist = df_dist.rename(columns={'virus':'sseqid_virus','genome_ncbi_acc':'sseqid_genome_ncbi_acc'})
+        df_dist['qseqid_virus'] = df_dist['qseqid_ID']
+        dists = pd.concat([ictv_dists, df_dist])
+        matrix = dists.pivot_table(columns='qseqid_ID', index='sseqid_genome_ncbi_acc', values='distance').reset_index()
+        matrix = matrix.set_index('sseqid_genome_ncbi_acc')
+        pb.update(task_id=t1, advance=25)
+        #matrix.to_csv('dist_matrix_phylim_raw.dist', header=True, index=True, sep =' ')
+        # Check dimensions of matrix
+        cols = matrix.columns.to_list()
+        indx = matrix.index.to_list()
+        cols_missing = list(set(indx) - set(cols))
+        idx_missing = list(set(cols) - set(indx))
+        new_row = pd.Series(np.full(shape=len(idx_missing), fill_value=np.nan), index = idx_missing)
+        indexes_insert = pd.DataFrame(index=new_row.index)
+        matrix = pd.concat([matrix, indexes_insert], axis=1)
+        pb.update(task_id=t1, advance=25)
         for col in cols_missing:
-            matrix = matrix.reset_index()
-            row_to_append = (matrix.shape[1]-1) * [1]
-            row_to_append.insert(0, col)
-            matrix.loc[matrix.shape[0]] = row_to_append
-            matrix = matrix.set_index('sseqid_genome')
-    if len(indx)>len(cols):
-        # insert extra index values as columns
-        for idx in idx_missing:
-            matrix[idx] = 1
-    # matrix = matrix.set_index('sseqid_genome')
-    #sort dataframe by index and column values
-    print(matrix.head())
-    sorted_index = sorted(matrix.index)
-    matrix = matrix.loc[sorted_index]
-    matrix = matrix[sorted_index]
-    matrix.columns = [''] * len(matrix.columns)
-    matrix.index.names = [len(matrix)]
-    # (),: - not permitted by fastme in index
-    matrix.index = matrix.index.str.replace('\,|\(|\)|\:','_', regex=True)
-    # fastme: taxa names must not be longer than 64 characters
-    matrix.index = matrix.index.str[:64]
-    np.fill_diagonal(matrix.values, 0)
-    matrix = matrix.fillna(1) \
-        .astype(float) \
-        .round(5) \
-        .clip(lower = 0)
-    matrix.to_csv('./crassify_output/dist_matrix_phylip.dist', header=True, index=True, sep =' ')
-
+            matrix[col] = 1
+        sorted_index = sorted(matrix.index)
+        matrix = matrix.loc[sorted_index]
+        matrix = matrix[sorted_index]
+        matrix.columns = [''] * len(matrix.columns)
+        matrix.index.names = [len(matrix)]
+        # (),: - not permitted by fastme in index
+        matrix.index = matrix.index.str.replace('\,|\(|\)|\:','_', regex=True)
+        # fastme: taxa names must not be longer than 64 characters
+        matrix.index = matrix.index.str[:64]
+        np.fill_diagonal(matrix.values, 0)
+        pb.update(task_id=t1, advance=25)
+        matrix = matrix.fillna(1) \
+            .astype(float) \
+            .round(5) \
+            .clip(lower = 0)
+        matrix.to_csv(f'{outpath}/phylip.dist', header=True, index=True, sep =' ')
+        pb.update(task_id=t1, advance=25)
     return matrix
 
 def to_newick(phylip_matrix, path_to_decenttree):
     print(f'{datetime.now()}: Calling DecentTree to convert PHYLIP to Newick format.')
     # ./decenttree -in /data/san/data1/users/linda/crAss_DB/ictv_proposal_seq_translations/dist_matrix_phylim.dist -t NJ -nt 10 -out dist_matrix_NJ.newick
-    p = subprocess.call([path_to_decenttree, "-i", phylip_matrix])
+    p = subprocess.call([path_to_decenttree, "-in", phylip_matrix])
     # check p output
 
 def plot_tree(path_to_newick, path_to_metadata):
@@ -205,7 +205,7 @@ if __name__ == "__main__":
     genome_prot_map = map_genome_to_proteome(args.proteins)
     matches = merge_metadata_matches(args.diamond_matches, genome_prot_map, args.db_metadata)
     dists = calc_dist(matches)
-    #phylip = to_phylip(dists)
+    phylip = to_phylip(dists, '/home/administrator/phd/crassify/databases/ictv_distances.csv')
     # tree = plot_tree('/home/administrator/phd/crass_DB/May_2023_317_proteomes/crassify/dist_matrix_phylim.newick', args.path_to_metadata)
 
 
